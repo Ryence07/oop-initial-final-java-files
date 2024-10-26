3a75eb4106a8b2d9e2ab386f77e2d80d1c8be56c
Ryence07 <ryences.cortez@gmail.com>
2024-10-26 21:49:17 +0800

    Update Main.java

--- Main.java
+++ Main.java
@@ -10,42 +10,42 @@ public class Main {
     public static void main(String[] args) {
 
         do {
-            switch (SystemFunctions.getCurrentMenu()) {
+            switch (SystemFunctions.getCurrentMenu()) { // Check the current menu to determine what to display
                 // The enum constants are named based on the class file of the menus
                 case FrontMenu: {
-                    FrontMenu.openMenu();
+                    FrontMenu.openMenu(); // Open the Front Menu
                     break;
                 }
                 case RoleSelection: {
-                    RoleSelection.openMenu();
+                    RoleSelection.openMenu(); // Open the Role Selection Menu
                     break;
                 }
                 case SupplierMenu: {
-                    SupplierMenu.openMenu();
+                    SupplierMenu.openMenu(); // Open the Supplier Menu
                     break;
                 }
                 case CheckSupply_Supplier: {
-                    CheckSupply_Supplier.openMenu();
+                    CheckSupply_Supplier.openMenu(); // Open the Check Supply Menu
                     break;
                 }
                 case AddSupply_Supplier: {
-                    AddSupply_Supplier.openMenu();
+                    AddSupply_Supplier.openMenu(); // Open the Add Supply Menu
                     break;
                 }
                 case Purchase_Buyer: {
-                    Purchase_Buyer.openMenu();
+                    Purchase_Buyer.openMenu(); // Open the Buyer Purchase Menu
                     break;
                 }
                 default: {
-                    System.out.println(SystemFunctions.getCurrentMenu());
+                    System.out.println(SystemFunctions.getCurrentMenu()); // Print the current menu for debugging
                     System.out.println("AN ERROR HAS OCCURED IN THE CODE. PLEASE DOUBLE CHECK ALL METHOD CALLS FOR SystemFunctions.changeMenu()");
-                    SystemFunctions.shutDownProgram();
+                    SystemFunctions.shutDownProgram(); // Shut down the program due to an error
                     break;
                 }
             }
 
 
-        } while (SystemFunctions.getProgramStatus());
+        } while (SystemFunctions.getProgramStatus()); // Continue running the program if the status is true
 
     }
 
