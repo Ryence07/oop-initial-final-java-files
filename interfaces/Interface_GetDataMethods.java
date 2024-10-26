4f62177f96e6b7475d1b59f09fc0a25ea8cc1aa9
Ryence07 <ryences.cortez@gmail.com>
2024-10-26 21:07:46 +0800

    Update Interface_GetDataMethods.java

--- interfaces/Interface_GetDataMethods.java
+++ interfaces/Interface_GetDataMethods.java
@@ -1,5 +1,5 @@
 package interfaces;
-
+// Interface to define methods for retrieving basic personal data
 public interface Interface_GetDataMethods {
     abstract String getFirstName();
     abstract String getLastName();
