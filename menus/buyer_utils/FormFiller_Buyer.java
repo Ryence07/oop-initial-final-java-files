59af7776e4ba7acaac9994035b67dfd023a21b73
Ryence07 <ryences.cortez@gmail.com>
2024-10-26 21:15:23 +0800

    Update FormFiller_Buyer.java
    comment

--- menus/buyer_utils/FormFiller_Buyer.java
+++ menus/buyer_utils/FormFiller_Buyer.java
@@ -1,14 +1,13 @@
 package menus.buyer_utils;
 
-
 import data.BuyerData;
 import menus.parent_classes.FormFiller;
 
-public class FormFiller_Buyer extends FormFiller{
+// A utility class for filling and submitting buyer information forms
+public class FormFiller_Buyer extends FormFiller {
     public static void openMenu() {
+        // Collects buyer details and fills them in BuyerData
         String[] details = FormFiller.fillFormBuyer();
         BuyerData.dataObj.fillDetails(details[0], details[1], details[2], details[3]);
-
-        
     }
 }
